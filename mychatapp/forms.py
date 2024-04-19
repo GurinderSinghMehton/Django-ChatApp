d9148f94fc881e6c725dda6b4539cf35f4abcc3c
Gurinder Singh <156795784+GurinderSinghMehton@users.noreply.github.com>
2024-04-19 21:42:25 +0530

    Initial commit

---
+++ mychatapp/forms.py
@@ -0,0 +1,9 @@
+from django import forms 
+from django.forms import ModelForm
+from .models import ChatMessage
+
+class ChatMessageForm(ModelForm):
+    body = forms.CharField(widget=forms.Textarea(attrs={"class": "forms", "rows": 3, "placeholder": "Type your message..."}))
+    class Meta:
+        model = ChatMessage
+        fields = ['body', ]
